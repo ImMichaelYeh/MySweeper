c68f3c40678502133c0fb348c3ccb33e42e24340
Michael Yeh <myeh2k@yahoo.com>
2021-06-10 14:51:58 -0700

    Fixed UI and formatting issues

--- src/Program.java
+++ src/Program.java
@@ -1,70 +1,158 @@
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+public class Program extends Application implements Tile.TileListener {
 
-public class Program extends Application implements Tile.TileListener{
-	
-	private final String GAME_TITLE = "MySweeper";
-	
-	private final Image FACESMILEY = new Image("res/smiley.png");
-	private final Image FACESHOCKED = new Image("res/shocked.png");
-	private final Image FACESUNGLASSES = new Image("res/sunglasses.png");
-	private final Image FACEDEAD = new Image("res/dead.png");
+    private final String GAME_TITLE = "MySweeper";
 
+    private Text flagsRemainingText;
+    private Button faceButton;
+    private Text timerText;
+
+    private final int HEIGHT = 16;
+    private final int WIDTH = 30;
+    private final int MINES = 99;
+    private final int CELLSIZE = 40;
+    private final int FACEBUTTONSIZE = 50;
+    private final int TOPBARHEIGHT = 100;
+
+    private final int STAGEWIDTH = CELLSIZE * WIDTH;
+    private final int STAGEHEIGHT = CELLSIZE * HEIGHT + TOPBARHEIGHT;
+
+    private final ImageView FACESMILEY = new ImageView("res/smiley.png");
+    private final ImageView FACESHOCKED = new ImageView("res/shocked.png");
+    private final ImageView FACESUNGLASSES = new ImageView("res/sunglasses.png");
+    private final ImageView FACEDEAD = new ImageView("res/dead.png");
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+	Game game = new Game(this, HEIGHT, WIDTH, MINES);
+
+	// Prepare scene graph with required nodes
+	VBox rootLayout = new VBox();
+	BorderPane topBar = new BorderPane();
+	StackPane flagsPane = new StackPane(), facePane = new StackPane(), timerPane = new StackPane();
+	GridPane minefield = new GridPane();
+
+	/** START TOP BAR **/
+	BackgroundFill topBarBackgroundFill = new BackgroundFill(Color.rgb(222, 222, 222), CornerRadii.EMPTY,
+		Insets.EMPTY);
+	Background topBarBackground = new Background(topBarBackgroundFill);
+
+	flagsRemainingText = new Text("099");
+	faceButton = new Button();
+	timerText = new Text("000");
+
+	flagsPane.getChildren().add(flagsRemainingText);
+	flagsPane.setPrefWidth(STAGEWIDTH * 0.25);
+	flagsRemainingText.setFont(Font.font("impact", FontWeight.BOLD, FontPosture.REGULAR, 50));
+	flagsRemainingText.setFill(Color.RED);
+	flagsRemainingText.setStroke(Color.BLACK);
+	topBar.setMinHeight(TOPBARHEIGHT);
+
+	facePane.getChildren().add(faceButton);
+	FACESMILEY.setFitHeight(FACEBUTTONSIZE);
+	FACESMILEY.setFitWidth(FACEBUTTONSIZE);
+	faceButton.setGraphic(FACESMILEY);
+
+	timerPane.getChildren().add(timerText);
+	timerPane.setPrefWidth(STAGEWIDTH * 0.25);
+	timerText.setFont(Font.font("impact", FontWeight.BOLD, FontPosture.REGULAR, 50));
+	timerText.setFill(Color.RED);
+	timerText.setStroke(Color.BLACK);
+
+	topBar.setLeft(flagsPane);
+	topBar.setCenter(facePane);
+	topBar.setRight(timerPane);
+	rootLayout.getChildren().add(topBar);
+	topBar.setBackground(topBarBackground);
+	/** END TOP BAR **/
+
+	/** START MINEFIELD **/
+	Tile[][] board = game.getBoard();
+	rootLayout.getChildren().add(minefield);
+	for (int row = 0; row < HEIGHT; row++) {
+	    for (int col = 0; col < WIDTH; col++) {
+		Node tile = board[row][col].getStackPane();
+		// minefield.setMargin(tile, new Insets(0.5,0.5,0.5,0.5));
+		minefield.add(tile, col, row); // for some reason, grid pane uses (col, row) for coordinates instead of
+					       // (row, col)
+	    }
+	}
+	/** END MINEFIELD **/
+
+	/*
+	 * Prepare a scene w/ required dimensions and add the scene graph (root node of
+	 * the scene) to it
+	 */
+	Scene primaryScene = new Scene(rootLayout, STAGEWIDTH, STAGEHEIGHT);
+	primaryStage.setTitle(GAME_TITLE);
+	primaryStage.setScene(primaryScene);
+	primaryStage.show();
+	// FIX RESIZING ISSUES
+    }
+
+    public static void main(String[] args) {
+	launch(args); // internally calls start()
+    }
+
+    public int getCellSize() {
+	return CELLSIZE;
+    }
+
+    public void setFaceToSmile() {
+    }
+
+    public void setFaceToSurprised() {
+    }
+
+    public void setFaceToSunglasses() {
+    }
+
+    public void setFaceToDead() {
+    }
+
+    public void setTimerText(long seconds) {
+	String timer = Long.toString(seconds);
+	while (timer.length() < 3)
+	    timer = "0" + timer;
+	if (timerText != null)
+	    timerText.setText(timer);
+    }
+
+    /**
+     * faceButton action listener
+     */
+    private class FaceButtonListener implements EventHandler<ActionEvent> {
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// Prepare scene graph with required nodes
-		VBox rootLayout = new VBox();
-		BorderPane topBar = new BorderPane();
-		GridPane minefield = new GridPane();
-		
-		/** Top bar **/
-		BackgroundFill topBarBackgroundFill = new BackgroundFill(Color.rgb(192,192,192), 
-				CornerRadii.EMPTY, Insets.EMPTY);
-		Background topBarBackground = new Background(topBarBackgroundFill);
-		Button face = new Button();
-		face.setPrefSize(80, 80);
+	public void handle(ActionEvent event) {
+	    // TODO Auto-generated method stub
 
-		topBar.setCenter(face);
-		
-		rootLayout.getChildren().add(topBar);
-		rootLayout.getChildren().add(minefield);
-		topBar.setBackground(topBarBackground);
-		
-		/* 
-		 * Prepare a scene w/ required dimensions and
-		 * add the scene graph (root node of the scene) to it
-		 */
-		Scene primaryScene = new Scene(rootLayout, 600, 300);
-		
-		// Prepare a stage and add the scene to the stage and display the contents
-		primaryStage.setTitle(GAME_TITLE);
-		primaryStage.setScene(primaryScene);
-		primaryStage.show();
-		
 	}
-	
-	public static void main(String[] args) {
-		launch(args); // internally calls start()
-	}
-	
-	public void setFaceToSmile() {}
-	public void setFaceToSurprised() {}
-	public void setFaceToSunglasses() {}
-	public void setFaceToDead() {}
-
+    }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -3,59 +3,67 @@ import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
-public class Tile extends StackPane{
-	
-	private static final Image[] NUMBER = { new Image("res/zero.png"), new Image("res/one.png"), new Image("res/two.png"), 
-											new Image("res/three.png"), new Image("res/four.png"), new Image("res/five.png"), 
-											new Image("res/six.png"), new Image("res/seven.png"), new Image("res/eight.png") };
-	private static final Image FLAG = new Image("res/flag.png");
-	private static final Image BADFLAG = new Image("res/flag.png");
-	
-	private static final Image MINE = new Image("res/mine.png");
-	private static final Image MINECLICKED = new Image("res/mineclicked.png");
-	
-	private static final Image BACKGROUND = new Image("res/background.png");
-	private static final Image BORDEREDBACKGROUND = new Image("res/borderedbackground.png");
-	
-	
-	private TileListener player;
-	
-	private StackPane stackpane;
-	private Button tileButton;
-	private ImageView number;
-	private ImageView background;
+public class Tile extends StackPane {
 
-	public Tile(TileListener player) {
-		this.player = player;
-		stackpane = new StackPane();
-		tileButton = new Button();
-		number = new ImageView(NUMBER[0]);
-		background = new ImageView(BACKGROUND);
-		
-		stackpane.getChildren().add(background);
-		stackpane.getChildren().add(number);
-		stackpane.getChildren().add(tileButton);
-	}
-	
-	public Tile(TileListener player, int tileNumber) {
-		this(player);
-		
-		if (tileNumber >= 0 && tileNumber <= 8) {
-			number.setImage(NUMBER[tileNumber]);
-		}
-	}
-	
-	public void setSize(int width) {
-		stackpane.setMaxSize(width, width);
-		tileButton.setMaxSize(width, width);
-		number.setFitHeight(width);
-		number.setFitWidth(width);
-	}
-	
-	public interface TileListener{
-		public void setFaceToSmile();
-		public void setFaceToSurprised();
-		public void setFaceToSunglasses();
-		public void setFaceToDead();
-	}
+    private static final ImageView[] NUMBER = { new ImageView("res/zero.png"), new ImageView("res/one.png"),
+	    new ImageView("res/two.png"), new ImageView("res/three.png"), new ImageView("res/four.png"),
+	    new ImageView("res/five.png"), new ImageView("res/six.png"), new ImageView("res/seven.png"),
+	    new ImageView("res/eight.png") };
+    private static final Image FLAG = new Image("res/flag.png");
+    private static final Image BADFLAG = new Image("res/flag.png");
+
+    private static final Image MINE = new Image("res/mine.png");
+    private static final Image MINECLICKED = new Image("res/mineclicked.png");
+
+    private static final Image BACKGROUND = new Image("res/background.png");
+    private static final Image BORDEREDBACKGROUND = new Image("res/borderedbackground.png");
+
+    private Program instance;
+
+    private StackPane stackPane;
+    private Button tileButton; // Possible images: FLAG
+    private ImageView middle; // Possible images: NUMBER, BADFLAG, MINE, MINECLICKED
+    private ImageView background; // Possible images: BORDEREDBACKGROUND
+
+    public Tile(Program instance) {
+	this.instance = instance;
+	stackPane = new StackPane();
+	tileButton = new Button();
+	middle = NUMBER[0];
+	background = new ImageView(BORDEREDBACKGROUND);
+	stackPane.getChildren().add(background);
+	stackPane.getChildren().add(middle);
+	stackPane.getChildren().add(tileButton);
+	setSize(instance.getCellSize());
+    }
+
+    public void setSize(int width) {
+	stackPane.setMinSize(width, width);
+	stackPane.setMaxSize(width, width);
+	stackPane.setPrefSize(width, width);
+
+	tileButton.setMinSize(width, width);
+	tileButton.setMaxSize(width, width);
+	tileButton.setPrefSize(width, width);
+
+	middle.setFitHeight(width);
+	middle.setFitWidth(width);
+
+	background.setFitHeight(width);
+	background.setFitWidth(width);
+    }
+
+    public StackPane getStackPane() {
+	return stackPane;
+    }
+
+    public interface TileListener {
+	public void setFaceToSmile();
+
+	public void setFaceToSurprised();
+
+	public void setFaceToSunglasses();
+
+	public void setFaceToDead();
+    }
 }
